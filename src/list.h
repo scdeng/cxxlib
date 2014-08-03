46c8ca368e7e742f430b3d7f22192803969a2aa0
scdeng <scdeng@163.com>
2014-08-03 22:07:33 +0800

    add iterator

--- src/list.h
+++ src/list.h
@@ -20,6 +20,7 @@
 #define LIST_H
 #include <stdlib.h>
 #include <iostream>
+#define DEBUG_MODE
 #define NON_ITERATOR
 //list node
 template <typename T> struct ListNode{
@@ -30,10 +31,130 @@ template <typename T> struct ListNode{
 };
 
 
-template <typename T> 
-class List{
+/*
+ *	template class List
+ *	List is bidirectional linked list
+ *	
+ *
+ */
+template <typename T>  class List{
+
+#ifdef DEBUG_MODE
+	template <class U> friend bool assert_list(const List<U> &);
+#endif
+
 	template <class U> friend std::ostream& operator<<(std::ostream&, const List<U>&);
+	//template <class Tp> friend class Iterator;
+
+	//iterator class of List
+	template <typename U> class Iterator{
+		template <typename TYPE> friend class List;
+		private:
+			ListNode<U> *cur;
+		public:
+
+			//default constructor
+			//dereference this iterator occurs an error
+			Iterator(): cur(NULL) { }
+
+			//construct Iterator at node
+			Iterator(ListNode<U> *node){
+				cur = node;
+			}
+
+			//copy constructor
+			Iterator(const Iterator &from){
+				cur = from.cur;
+			}
+
+			//copy assignment
+			Iterator& operator=(const Iterator &that){
+				this->cur = that.cur;
+				return *this;
+			}
+			
+			//compare 
+			bool operator!=(const Iterator &that) const {
+				return cur != that.cur;
+			}
+
+			bool operator==(const Iterator &that) const {
+				return cur == that.cur;
+			}
+
+			//dereference returning an object 
+			U operator*() const {
+				return cur->data;
+			}
+			
+			//I don't know what happens
+			//Just write like stl
+			U * operator->() const {
+				return (U*) cur->data;
+			}
+			
+			//prefix ++operator
+			Iterator& operator++(){
+				cur = cur->next;
+				return *this;
+			}
+			//suffix ++operator
+			Iterator operator++(int){
+				Iterator tmp(*this);
+				cur = cur->next;
+				return tmp;
+			}
+			
+			//prefix -- operator
+			Iterator& operator--(){
+				cur = cur->prev;
+				return *this;
+			}
+			//suffix -- operator
+			Iterator operator--(int){
+				Iterator tmp(*this);
+				cur = cur->prev;
+				return tmp;
+			}
+			
+//			//move n items
+//			//n can be any integer
+//			//move forward when n is positive
+//			//move backward when n i negative
+//			//no move when n is 0
+//			Iterator& advance(int n){
+//				//move forward
+//				if (n > 0){
+//					while( cur->next && n-- > 0){
+//						cur = cur->next;
+//					}
+//					
+//				}else if( n < 0 ){
+//				//move backward
+//
+//				}else{
+//				//no move
+//				}
+//			}
+
+			U& next(){	
+				cur = cur->next;
+				return cur->prev->data;
+			}
+
+			//java style iterator
+			//
+			bool hasNext()const{
+				return cur->next != NULL;
+			}
+	};
+
+
+
+
+
 	private:
+
 	//head
 	ListNode<T> *head;
 	//tail 
@@ -41,65 +162,107 @@ class List{
 	//no. of nodes
 	size_t sz;
 
-	void copy_list(const List &from){
-
-		//get from head
-		ListNode<T> *node = from.head;
-
-		if(!node){
-			head = NULL;
-			tail = NULL;
-		}else{
-			//non-empty list
-
-			//create a new ListNode as head and tail
-			head = new ListNode<T>(node->data);
-
-			tail = head;
-			node = node->next;
-
-			//deep copy all data
-			while( node ){
-				//create a new node and append
-				ListNode<T> *tmp = new ListNode<T>(node->data);
-				tail->next = tmp;
-				tmp->prev = tail;
-
-				//move p to next
-				tail = tail->next;
-				node = node->next;
-			}
+	void copy_list(const List &from){	
+		//get from's element
+		ListNode<T> *pf = from.head->next;
+		//while != from's tail
+		//deep copy ListNode
+		ListNode<T> *node = NULL;
+		while( pf != from.tail){
+			//create a new node using pf's data
+			node = new ListNode<T>(pf->data);
+			//insert to last
+			insert_after(tail->prev, node);
+			//move pf to next
+			pf = pf->next;
+			node = NULL;
 		}
+		//set sz
 		sz = from.sz;
 	}
-	
+
 
 	//destroy will release all data node 
 	//and set head,tail to be 0
 	//set sz to be 0
 	void destroy(){
-		//get head
-		ListNode<T> *p = head;
-		ListNode<T> *q = head;
-		//free every node
-		while(p){
+		ListNode<T> *p = head->next;
+		ListNode<T> *q = head->next;
+		while(p != tail){
 			p = p->next;
 			delete q;
 			q = p;
 		}
-		//set head tail sz to 0
-		head = NULL;
-		tail = NULL;
+
+		head->next = tail;
+		tail->prev = head;
 		sz = 0;
 	}
 
+	//remove node p helper 
+	void pop_node(ListNode<T> *p){
+		p->next->prev = p->prev;
+		p->prev->next = p->next;
+		p->next = p->prev = NULL;
+		delete p;
+	}
+
+	//insert node after p
+	void insert_after(ListNode<T> *p, ListNode<T> *node){
+		node->next = p->next;
+		node->prev = p;
+		p->next->prev = node;
+		p->next = node;
+	}
+	
+	//insert node before p
+	void insert_before(ListNode<T> *p, ListNode<T> *node){
+		node->prev = p->prev;
+		node->next = p;
+		p->prev->next = node;
+		p->prev = node;
+	}
+	bool remove(ListNode<T> *node){
+		//ignore head and tail
+		if(node == head || node == tail){
+			return false;
+		}
+		//relink next and previous node
+		node->next->prev = node->prev;
+		node->prev->next = node->next;
+		delete node;
+		return true;
+	}
+
 	public:
-	List():head(NULL), tail(NULL), sz(0){}
+	typedef Iterator<T> iterator;
+
+	List(){
+		head = new ListNode<T>(T());
+		tail = new ListNode<T>(T());
+		head->next = tail;
+		tail->prev = head;
+		sz = 0;
+	}
 
 	List( const List &from){
+		head = new ListNode<T>(T());
+		tail = new ListNode<T>(T());
+		head->next = tail;
+		tail->prev = head;
+		//copy_list(from);
+		//not complete
 		copy_list(from);
 	}
 
+	//non-virtual destructor
+	~List(){
+		destroy();
+		delete head;
+		delete tail;
+	}
+
+	//assignment operator
 	List& operator=(const List &that){
 		//if this is not equal to that
 		//destroy this and copy that to this
@@ -111,26 +274,17 @@ class List{
 		}
 		//if this is equal to that 
 		//do nothing
+		return &this;
 	}
-	
+
 	//insert an item into head
 	void push_front(const T &t){
 		//create a new node
 		ListNode<T> *node = new ListNode<T>(t);
-		
-		//if list is empty
-		//set head tail
-		if( empty() ){
-			head = tail = node;
-		}else{
-		//if list is not empty
-		//insert 
-			//link new node to list
-			node->next = head;
-			head->prev = node;
-			//set head to be new node
-			head = node;
-		}
+
+		//insert after head
+		insert_after(head, node);
+
 		//increament sz
 		++sz;
 	}
@@ -139,19 +293,10 @@ class List{
 	void push_back( const T &t){
 		//create a new node
 		ListNode<T> *node = new ListNode<T>(t);
-		//if this is empty
-		//set head and tail to the new node
-		if (empty()){
-			head = tail = node;
-		}else{
 
-			//if this is non-empty
-			tail->next = node;
-			node->prev = tail;
+		//insert after tail's prev
+		insert_after(tail->prev, node);
 
-			//append and update tail
-			tail = node;
-		}
 		//increate sz
 		++sz;
 	}
@@ -169,28 +314,18 @@ class List{
 			return false;
 		}else{
 			//if not empty
-			ListNode<T> *p = head;
-			//move head to next
-			head = head->next;
-
-			//delete head 
-			delete p;
+			//get node to be popped
+			ListNode<T> *p = head->next;
+			//pop it
+			pop_node(p);
+			//avoid dangling pointer
 			p = NULL;
-			
-			//if head is null then tail should be null
-			if( !head ){
-				tail = head;
-			}else{
-				// if head is not null
-				// set head's prev to be null
-				head->prev = NULL;	
-			}
 		}
 		//decrease sz
 		--sz;
 		return true;
 	}
-	
+
 	//pop the last element
 	bool pop_back(){
 		//if list is empty
@@ -199,28 +334,22 @@ class List{
 			std::cerr << "bad call pop_back on empty list..." << std::endl;
 			return false;
 		}else{
-			//non-empty
 
-			//move tail to previous node
-			ListNode<T> *p = tail;
-			tail = tail->prev;
-			//delete last element
-			delete p;
+			//create a new node
+			ListNode<T> *p = tail->prev;
+
+			//delete node p
+			pop_node(p);
+
+			//avoid dangling pointer
 			p = NULL;
-			//check if head has been deleted
-			if( !tail ){
-				//set head to NULL
-				head = tail;
-			}else{
-				//set tail's next to NULL
-				tail->next = NULL;
-			}
+
 		}
 		//decrease sz
 		--sz;
 		return true;
 	}
-	
+
 	//return the first element
 	//calling the on an empty list will cause undefined behavior
 	T front() const {
@@ -231,11 +360,11 @@ class List{
 			std::cerr << "data returned is not defined..." << std::endl;
 			return T();
 		}else{
-		//return head's data
-			return head->data;
+			//return head's data
+			return head->next->data;
 		}
 	}
-	
+
 	//return the last element
 	//calling the function on empty list will cause undefined behavior
 	T back()const {
@@ -247,41 +376,168 @@ class List{
 			return T();
 		}else{
 			//return head's data
-			return head->data;
+			return tail->prev->data;
+		}
+	}
+
+	//insert 
+	void insert( iterator pos, const T &t){
+		//create new node
+		ListNode<T> *node = new ListNode<T>(t);
+		//get cur;
+		ListNode<T> *p = pos.cur;
+		//insert before cur
+		insert_after(p->prev, node);
+		//increase sz
+		++sz;
+	}
+	
+	//insert [beg...end) before pos
+	template <typename InputIter>
+	void insert( iterator pos, InputIter beg, InputIter end){
+		ListNode<T> *p = pos.cur;
+		ListNode<T> *node = NULL;
+		InputIter it = beg;
+		while(it != end){
+			node = new ListNode<T>(*it);
+			insert_before(p, node);
+			++sz;
 		}
 	}
 	
+	void insert(iterator pos, int n, const T &t){
+		//get cur
+		ListNode<T> *p = pos.cur;
+		//create new node
+		ListNode<T> *node = NULL;
+		//increase sz by n
+		sz += n;
+		//insert n times
+		while ( n-- > 0 ){
+			//create new node 
+			node = new ListNode<T>(t);
+			insert_before(p, node);
+		}
+	}
+
+
+	//remove 
+	iterator remove(iterator pos){
+		iterator next_iter(pos.cur->next);
+		//get cur
+		ListNode<T> *p = pos.cur;
+		//remove node
+		if (remove(p)){
+			--sz;
+			p=NULL;
+			return next_iter;
+		}
+		return pos;
+	}
+
+	//remove
+	iterator remove(iterator beg, iterator end){
+		//
+		iterator it = beg;
+		//remove all
+		while (it != end){
+			it = remove(it);
+			
+		}
+		return it;
+	}
+
+
 	//clear all element
 	void clear(){
 		//destroy it
 		destroy();
 	}
 
-
-
+	//first element
+	iterator begin(){
+		return Iterator<T>(head->next);
+	}
+		
+	//end iterator
+	iterator end(){
+		return Iterator<T>(tail);
+	}
 
 	//return size of list
 	size_t size() const {return sz;}
-	
+
 	bool empty() const { return sz == 0; }
-
-
-
 };
 
 template <class U> std::ostream& operator<<(std::ostream &os, const List<U> &list){
+	if (list.head->next == list.tail){
+		os <<"{}"<<std::endl;
+		return os;
+	}
 	os << "{";
-	ListNode<U> *p = list.head;
-	while( p != list.tail ){
+	ListNode<U> *p = list.head->next;
+	while( p != list.tail->prev ){
 		os << p->data <<", ";
 		p = p->next;
 	}
-	if(list.tail){
-		os<< list.tail->data;
+	if(list.tail->prev != list.head){
+		os<< list.tail->prev->data;
 	}
 	os<< "}" <<std::endl;
 	return os;
 }
 
+template <class U>  bool assert_list(const List<U> &list){
+	size_t n = 0;
+	ListNode<U> *p = list.head;
+	//ListNode<U> *q = list.tail;
+	if( list.tail->next != NULL){
+		std::cerr << "tail's next is not null..." << std::endl;
+		return false;
+	}
 
+	while(p){
+		++n;
+		//check p's next and p's previous
+		//p is head
+		if(p == list.head){
+			if( p->prev != NULL ){
+				std::cerr << "head's prev is not null..." <<std::endl;
+			}
+
+			if (p->next->prev != p){
+				std::cerr << "not link back..." <<std::endl;
+				return false;
+			}
+		}else if(p == list.tail){
+			//p is tail
+			if( p->prev->next != p){
+				std::cerr << "not link..." << std::endl;
+				std::cerr << "data = " << p->data << std::endl;
+				return false;
+			}
+		}else{
+			if( p->next->prev != p ){
+				std::cerr << "not link back..." <<std::endl;
+				std::cerr << "data = " << p->data << std::endl;
+				return false;
+			}
+			if( p->prev->next != p){
+				std::cerr << "not link..." << std::endl;
+				std::cerr << "data = " << p->data << std::endl;
+				return false;
+			}
+		}
+		p = p->next;
+	}
+
+	if (n != list.sz + 2){
+		std::cerr<< "no. of element is incorrect..." <<std::endl;
+		std::cerr<< "n = " << n << std::endl;
+		std::cerr << "sz = " << list.sz << std::endl;
+		return false;
+	}
+	return true;
+}
 #endif
